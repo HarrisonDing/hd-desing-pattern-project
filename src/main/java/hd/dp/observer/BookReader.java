c7d169c3fc64522f5d1f721d6438ccac1bb24dd9
Harrison.Ding <dinglinbin@gmail.com>
2018-10-10 21:57:57 +0800

    update observer pattern with object transfer.

--- src/main/java/hd/dp/observer/BookReader.java
+++ src/main/java/hd/dp/observer/BookReader.java
@@ -4,10 +4,14 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class BookReader implements Observer {
+	private Article article = null;
 
 	@Override
 	public void update(Observable o, Object arg) {
+		article = (Article) arg;
 		System.out.println(String.format("Author has published a new book: %s", "go to read now"));
+		System.out.println(
+				String.format("Read a new book, title: %s, content: %s", article.getTitle(), article.getContent()));
 	}
 
 }
--- src/main/java/hd/dp/observer/BookUser.java
+++ src/main/java/hd/dp/observer/BookUser.java
@@ -5,11 +5,11 @@ import java.util.Observable;
 public class BookUser extends Observable {
 
 	public void publishArticle(String title, String content) {
+		System.out.println(String.format("Author publish a new article, title: %s, content: %s", title, content));
 		Article article = new Article();
 		article.setTitle(title);
 		article.setContent(content);
 		this.setChanged();
-		this.notifyObservers();
-		System.out.println(String.format("Author publish a new article, title: %s, content: %s", title, content));
+		this.notifyObservers(article);
 	}
 }
